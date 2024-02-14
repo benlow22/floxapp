2dc820bd4cddd9db5a849f60c029c93ff590b14c
benlow22 <ben2.2low@gmail.com>
2024-02-14 18:08:08 -0500

    format feed box

--- blank-mobile-app/components/FeedCard.tsx
+++ blank-mobile-app/components/FeedCard.tsx
@@ -4,16 +4,23 @@
 - post content
 - like, comment, save, 
  */
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import userData from "./../test-data/feedData.json";
 import { Avatar } from "./Avatar";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export const FeedCard = ({ data }: any) => (
 	<View style={styles.container}>
 		<View style={styles.userData}>
 			<Avatar avatarImage={data.avatarImage} />
 			<Text style={styles.username}>{data.userName}</Text>
+			<FontAwesome
+				name="ellipsis-v"
+				size={25}
+				style={styles.ellipsis}
+			/>
 		</View>
+		<Text style={styles.postBody}>{data.post}</Text>
 	</View>
 );
 
@@ -21,7 +28,8 @@ const styles = StyleSheet.create({
 	avatar: {
 		borderRadius: 10,
 	},
-
+	postBody: { marginTop: 15 },
+	ellipsis: { position: "absolute", right: 5, top: 5, color: "#999999" },
 	container: {
 		borderBottomColor: "black",
 		borderWidth: 1,
